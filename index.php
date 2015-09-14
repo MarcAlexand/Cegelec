4ee9a64ea817bbf4089d525cb3c527354e9d3eec
Donny van Walsem <donnehvw@gmail.com>
2015-09-14 14:04:08 +0200

    Fixed syntax errors in index

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 ob_start();
 include_once '/defs/constants.php';
@@ -8,7 +7,7 @@ include_once FILE_M_LOGINSYSTEEM;
 include_once FILE_DEFS_RECHTCONSTANS;
 $rechten = new RechtConstants();
 $reflection = new ReflectionClass($rechten);
-$recht_array= $reflection->getConstants();
+$recht_array = $reflection->getConstants();
 $login = new Login;
 
 $gebruiker = new Gebruiker(new DbGebruiker());
@@ -16,77 +15,56 @@ $gebruiker = new Gebruiker(new DbGebruiker());
 if (isset($_POST['logout'])) {
     $login->logout();
     exit;
-}                       
+}
 if (!$login->isloggedin()) {
-    header("Location: ".FILE_V_LOGINSCHERM);
+    header("Location: " . FILE_V_LOGINSCHERM);
     exit;
 } else {
     $recht = new GebruikerRecht();
     ?>
     <!DOCTYPE html>
     <html lang="nl">
-        <head>
-            <title>Cegelec</title>
-            <link rel="stylesheet" href="style/style.css" />
-            <meta charset="UTF-8">
-        </head>
-       
-        <header>
-                    <span id="logo">
-                        <a href="<?php echo URL_DASHBOARD ?>">
-                            <img src="img/actemium.png"/>
-                        </a>
-                    </span>
-                </header>
-        <body>
-            <div id="main">
-                
-                <?php
-                    if (isset($_GET['page'])) {
-                        switch ($_GET['page']) {
-                            case "dashboard":
-                                include FILE_V_DASHBOARD;
-                                break;
-                            case "gebruikers":
-                                include FILE_V_GEBRUIKERSOVERZICHT;
-                                break;
-                            case "opleiding":
-                                include FILE_V_OPLEIDING;
-                                break;
-                            case "locatie":
-                                include FILE_V_LOCATIE;
-                                break;
-                            case "leerling":
-                                include FILE_V_LEERLING;
-                                break;
-                    }} else {
-                        include FILE_V_DASHBOARD;
-                    }
-                ?>
-            </div>
-            <footer>
-                <p></p>
-            </footer>
-        </body>
-    </html>
-<?php
-}
-?>
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
     <head>
+        <title>Cegelec</title>
+        <link rel="stylesheet" href="style/style.css"/>
         <meta charset="UTF-8">
-        <title></title>
     </head>
     <body>
+        <header>
+            <span id="logo">
+                <a href="<?php echo URL_DASHBOARD ?>">
+                    <img src="img/actemium.png"/>
+                </a>
+            </span>
+        </header>
+    <div id="main">
         <?php
-        // put your code here
+        if (isset($_GET['page'])) {
+            switch ($_GET['page']) {
+                case "dashboard":
+                    include FILE_V_DASHBOARD;
+                    break;
+                case "gebruikers":
+                    include FILE_V_GEBRUIKERSOVERZICHT;
+                    break;
+                case "opleiding":
+                    include FILE_V_OPLEIDING;
+                    break;
+                case "locatie":
+                    include FILE_V_LOCATIE;
+                    break;
+                case "leerling":
+                    include FILE_V_LEERLING;
+                    break;
+            }
+        } else {
+            include FILE_V_DASHBOARD;
+        }
         ?>
+    </div>
     </body>
-</html>
+    </html>
+    <?php
+}
+?>
 
