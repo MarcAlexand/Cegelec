0dd9005ced38aa9cb16830f68553da8a31416845
Marc de Boer <BB2Media@MacBook-Pro-van-Marc.local>
2015-10-01 13:41:40 +0200

    Locatie toegevoegd

--- app/defs/db_constants.php
+++ app/defs/db_constants.php
@@ -10,7 +10,7 @@
 /* Database inlog gegevens */
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "1310Semnb");
 define("DB_NAME", "cegelec");
 
 
