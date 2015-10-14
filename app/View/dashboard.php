30a0c5a69386bcb4b4f7cf7fc8949cead23a205a
t-bie <t-bie@10.0.3.72>
2015-10-14 12:56:19 +0200

    Dashboard nieuwe stijl toegevoegd

--- app/View/dashboard.php
+++ app/View/dashboard.php
@@ -2,33 +2,33 @@
         <div id="dashboard">
             <div id="dashboardbutton">
                 <a href="<?php echo URL_USER_BEHEER; ?>">
-                    <img src="img/userbeheerbutton.png">
+                    <img src="img/gebruikers.png">
                     </br>
                     <?php echo URL_WEERGEVEN_USER_BEHEER; ?> 
                 </a>
             </div>
             <div id="dashboardbutton">
                 <a href="<?php echo URL_OPLEIDING_BEHEER; ?>">
-                    <img src="img/opleidingtoevoegen.png">
+                    <img src="img/opleidingen.png">
                     </br>
                     <?php echo URL_WEERGEVEN_OPLEIDING_BEHEER; ?> 
                 </a>
             </div>
             <div id="dashboardbutton">
                 <a href="<?php echo URL_LEERLING_BEHEER; ?>">
-                    <img src="img/userbeheerbutton.png">
+                    <img src="img/leerling.png">
                     </br>
                     <?php echo URL_WEERGEVEN_OVERZICHT_LEERLING; ?>
                 </a>
             </div>
             <div id="dashboardbutton">
                 <a href="<?php echo URL_LOCATIE_BEHEER; ?>">
-                    <img src="img/userbeheerbutton.png">
+                    <img src="img/locatie.png">
                     </br>
                     <?php echo URL_WEERGEVEN_OVERZICHT_LOCATIE; ?>
                 </a>
             </div>
-            <div id="dashboardbutton">
+            <div id="dashboardbutton" style="margin: 0 auto; width: 100%;">
                 <form method="POST" id="logout" name="logout">
                     <input type="hidden"  name="logout" value="true">
                     <input type="image" src="img/uitloggenbut.png"> 
